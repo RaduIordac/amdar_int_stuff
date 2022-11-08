c1a1aa9da7e765e47e654bd9baed4a30059bc66d
Radu Iordache <radu_iordache_83@yahoo.com>
2022-11-08 11:10:19 +0200

    Add Radu 2 second commit

--- TestgitConsoleApp1/Program.cs
+++ TestgitConsoleApp1/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Radu 1 commit");
+Console.WriteLine("Radu 2 commit");
+Console.WriteLine("Radu 2 second commit");
