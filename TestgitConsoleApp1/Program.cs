e0449aee813dab194dcc47dac05e4bdd5643d6e8
Radu Iordache <radu_iordache_83@yahoo.com>
2022-11-08 11:15:19 +0200

    a

--- TestgitConsoleApp1/Program.cs
+++ TestgitConsoleApp1/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Radu 1 commit");
+Console.WriteLine("Radu 1 second  commit");
